b1a84bf41034bfbf304fa50d4a8b4385f108fe6e
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-11 11:39:43 +0000

    refmt CardSource

--- src/components/presentational/CardSource.js
+++ src/components/presentational/CardSource.js
@@ -6,24 +6,22 @@ import copy from '../../js/data/copy.json'
 const CardSource = ({ source, language, isLoading, error }) => {
   const source_lang = copy[language].cardstack.source
 
-  if (isLoading) {
-    return <Spinner />
-  }
-
   function renderSource() {
-    return (
+    return source.error ? (
+      <div><small>{source.error}</small></div>
+    ) : (
       <div><p>TODO: display source properly.</p></div>
     )
+  }
+
+  function renderContent() {
+    return isLoading ? <Spinner/> : renderSource()
   }
 
   return (
     <div className="card-col card-cell source">
       <h4>{source_lang}: </h4>
-      {source.error ? (
-        <div><small>{source.error}</small></div>
-      ) : (
-        renderSource()
-      )}
+      {renderContent()}
     </div>
   )
 }
