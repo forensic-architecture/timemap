cabb81d75bfd5a51fb3ca0dcecb4f7ebdbd71703
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-11 10:37:52 +0000

    loading in CardSource when flag set

--- src/components/presentational/CardSource.js
+++ src/components/presentational/CardSource.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from 'react'
+import Spinner from './Spinner'
 
-import copy from '../../js/data/copy.json';
+import copy from '../../js/data/copy.json'
 
 const CardSource = ({ source, language, isLoading }) => {
-    const source_lang = copy[language].cardstack.source;
-    if (!source) source = copy[language].cardstack.unknown_source;
-    console.log(isLoading)
+  const source_lang = copy[language].cardstack.source
+  if (!source) source = copy[language].cardstack.unknown_source
 
-    return (
-        <div className="card-row card-cell source">
-            <h4>{source_lang}: </h4>
-            <p><small>{source}</small></p>
-        </div>
-    );
+  const content = isLoading ? (
+    <Spinner />
+  ) : (
+    <div><small>{source}</small></div>
+  )
+
+  return (
+    <div className="card-col card-cell source">
+      <h4>{source_lang}: </h4>
+      {content}
+    </div>
+  )
 }
 
-export default CardSource;
+export default CardSource
