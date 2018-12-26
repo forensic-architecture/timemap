c20324c267605c264726ef70b076cbd8b8d4869b
Lachlan Kermode <lachiekermode@gmail.com>
2018-12-26 12:13:16 +0100

    update example

--- example.config.js
+++ example.config.js
@@ -4,16 +4,17 @@ module.exports = {
   EVENT_EXT: '/api/example/export_events/deeprows',
   CATEGORY_EXT: '/api/example/export_categories/rows',
   NARRATIVE_EXT: '/api/example/export_narratives/rows',
-  SOURCES_EXT: '/api/example/export_events/ids',
-  SITES_EXT: '/api/example/export_sites/rows',
+  SOURCES_EXT: '/api/example/export_sources/ids',
   TAGS_EXT: '/api/example/export_tags/tree',
+  SITES_EXT: '/api/example/export_sites/rows',
   MAP_ANCHOR: [31.356397, 34.784818],
   INCOMING_DATETIME_FORMAT: '%m/%d/%YT%H:%M',
-  MAPBOX_TOKEN: 'SOME_MAPBOX_TOKEN',
+  MAPBOX_TOKEN: 'pk.EXAMPLE_MAPBOX_TOKEN',
   features: {
     USE_TAGS: false,
     USE_SEARCH: false,
-    USE_SITES: false,
+    USE_SITES: true,
     USE_SOURCES: true
   }
 }
+
