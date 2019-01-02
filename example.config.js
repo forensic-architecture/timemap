81db5481ca3b835372e8431ba5b3af245da55003
Lachlan Kermode <lachiekermode@gmail.com>
2019-01-02 15:44:31 +0000

    correct sources URL

--- example.config.js
+++ example.config.js
@@ -4,7 +4,7 @@ module.exports = {
   EVENT_EXT: '/api/example/export_events/deeprows',
   CATEGORY_EXT: '/api/example/export_categories/rows',
   NARRATIVE_EXT: '/api/example/export_narratives/rows',
-  SOURCES_EXT: '/api/example/export_sources/ids',
+  SOURCES_EXT: '/api/example/export_sources/deepids',
   TAGS_EXT: '/api/example/export_tags/tree',
   SITES_EXT: '/api/example/export_sites/rows',
   MAP_ANCHOR: [31.356397, 34.784818],
