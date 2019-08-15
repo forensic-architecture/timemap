0b9940f91385ddc1cb636ce82f773858a63cf67d
Lachlan Kermode <lachiekermode@gmail.com>
2019-08-15 15:50:40 +0100

    clean SCSS and add browser notify

--- src/index.jsx
+++ src/index.jsx
@@ -10,3 +10,24 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('explore-app')
 )
+
+// Expressions from https://exceptionshub.com/how-to-detect-safari-chrome-ie-firefox-and-opera-browser.html
+
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+// Safari 3.0+ "[object HTMLElementConstructor]"
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+if (isSafari || isEdge || isIE || isOpera) {
+  alert('Please view this website in Firefox or Chrome for best viewing. It is untested in your browser.')
+}
