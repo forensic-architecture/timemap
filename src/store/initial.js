e0cedc4564d1155c32da5d5803e50a73e19bc997
Lachlan Kermode <lachiekermode@gmail.com>
2020-04-03 18:14:15 +0200

    WIP: lots of messy code for better timelines

--- src/store/initial.js
+++ src/store/initial.js
@@ -60,12 +60,12 @@ const initial = {
     },
     timeline: {
       dimensions: {
-        height: 250,
+        height: 1000,
         width: 0,
         marginLeft: 100,
         marginTop: 15,
         marginBottom: 60,
-        contentHeight: 200,
+        contentHeight: 900,
         width_controls: 100
       },
       range: [
