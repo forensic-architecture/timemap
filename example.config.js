f70957da1b50671714b69518c59648ecb1a473f6
Lachlan Kermode <lachiekermode@gmail.com>
2020-06-08 19:35:33 +0200

    update example

--- example.config.js
+++ example.config.js
@@ -2,14 +2,15 @@ module.exports = {
   title: 'example',
   display_title: 'example',
   SERVER_ROOT: 'http://localhost:4040',
-  EVENT_EXT: '/api/example/export_events/deeprows',
-  CATEGORY_EXT: '/api/example/export_categories/rows',
-  NARRATIVE_EXT: '/api/example/export_narratives/rows',
+  EVENTS_EXT: '/api/example/export_events/deeprows',
+  CATEGORIES_EXT: '/api/example/export_categories/rows',
+  FILTERS_EXT: '/api/example/export_filters/tree',
   SOURCES_EXT: '/api/example/export_sources/deepids',
-  TAGS_EXT: '/api/example/export_tags/tree',
-  SITES_EXT: '/api/example/export_sites/rows',
-  SHAPES_EXT: '/api/example/export_shapes/columns',
-  INCOMING_DATETIME_FORMAT: '%m/%d/%YT%H:%M',
+  NARRATIVE_EXT: '',
+  SITES_EXT: '',
+  SHAPES_EXT: '',
+  DATE_FMT: 'MM/DD/YYYY',
+  TIME_FMT: 'hh:mm',
   // MAPBOX_TOKEN: 'pk.YOUR_MAPBOX_TOKEN',
   store: {
     app: {
@@ -19,26 +20,22 @@ module.exports = {
     },
     ui: {
       style: {
-        categories: {
-          // alpha: 'blue',
-          beta: 'orange'
-        },
-        shapes: {},
-        narratives: {},
-        selectedEvent: {}
         // tiles: 'your-mapbox-account-name/x5678-map-id'
       }
     },
     features: {
+      USE_CATEGORIES: false,
+      CATEGORIES_AS_FILTERS: false,
+      USE_FILTERS: false,
+      USE_SOURCES: true,
+      GRAPH_NONLOCATED: false,
+      HIGHLIGHT_GROUPS: false,
+
       USE_COVER: false,
-      USE_TAGS: false,
       USE_SEARCH: false,
       USE_SITES: true,
-      USE_SOURCES: true,
       USE_SHAPES: false,
-      CATEGORIES_AS_TAGS: true,
-      GRAPH_NONLOCATED: false,
-      HIGHLIGHT_GROUPS: false
+
     }
   }
 }
