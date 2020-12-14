19c8b7179ee54d8021e68f69ec568d3d812a80a1
Zac Ioannidis <zacoppotamus@gmail.com>
2020-12-14 14:37:35 +0000

    Revert to default config for test suite

--- config/jest/setEnvVars.js
+++ config/jest/setEnvVars.js
@@ -1,2 +1,2 @@
-const envConfig = require("../../" + process.env.CONFIG);
+const envConfig = require("../../" + (process.env.CONFIG || 'config.js'));
 process.env = { ...process.env, ...envConfig };
