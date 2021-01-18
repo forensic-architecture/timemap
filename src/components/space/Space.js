65a4f92718c4a67878a5047786180ca62570bdf6
Lachlan Kermode <lachiekermode@gmail.com>
2021-01-18 19:35:39 +1300

    comment out 3d for now

--- src/components/space/Space.js
+++ src/components/space/Space.js
@@ -1,11 +1,11 @@
 import React from "react";
 import MapCarto from "./carto/Map";
-import Map3d from "./3d/Map";
+// import Map3d from "./3d/Map";
 
 const Space = (props) => {
   switch (props.kind) {
-    case "3d":
-      return <Map3d {...props} />;
+    // case "3d":
+    //   return <Map3d {...props} />;
     default:
       return <MapCarto {...props} />;
   }
