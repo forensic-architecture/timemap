283858faa4d8780d7c31188d407abe00147171a5
Lachlan Kermode <lachiekermode@gmail.com>
2022-04-04 16:49:28 -0400

    :lipstick: cherry-pick

--- src/components/space/carto/atoms/SatelliteOverlayToggle.js
+++ src/components/space/carto/atoms/SatelliteOverlayToggle.js
@@ -4,15 +4,11 @@ import { language } from "../../../../common/utilities";
 import mapImg from "../../../../assets/satelliteoverlaytoggle/map.png";
 import satImg from "../../../../assets/satelliteoverlaytoggle/sat.png";
 
-const SatelliteOverlayToggle = ({ toggleView, reset, isUsingSatellite }) => {
-  const buttonClass = isUsingSatellite
-    ? "satellite-overlay-toggle-button satellite-overlay-toggle-map"
-    : "satellite-overlay-toggle-button satellite-overlay-toggle-sat";
-  const bgImage = isUsingSatellite ? mapImg : satImg;
-  const label = isUsingSatellite
-    ? copy[language].tiles.default
-    : copy[language].tiles.satellite;
-
+const SatelliteOverlayToggle = ({
+  switchToSatellite,
+  reset,
+  isUsingSatellite,
+}) => {
   return (
     <div id="satellite-overlay-toggle" className="satellite-overlay-toggle">
       {isUsingSatellite ? (
