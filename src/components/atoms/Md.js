886c61b52056e6d9a0c20a657c39d5dd0ebff4a7
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-04-09 16:03:27 +0200

    fix: correctly import `marked`

--- src/components/atoms/Md.js
+++ src/components/atoms/Md.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import marked from "marked";
+import { marked } from "marked";
 
 class Md extends React.Component {
   constructor(props) {
